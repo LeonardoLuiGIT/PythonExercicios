498a44dcc20ac46c97412c3c44256edc4fbc1688
Leo Lui <luileonardojose@gmail.com>
2026-06-09 19:42:25 -0300

    Atualizacap do projeto

--- Mundo2/ex052.py
+++ Mundo2/ex052.py
@@ -2,13 +2,17 @@
 
 num = int(input("Digite um número: "))
 
-total = 0
+tot = 0
 
 for c in range(1, num + 1):
     if num % c == 0:
-        total += 1
-
-if total == 2:
-    print(f"O número {num} é primo!")
+        print('\033[33m', end='')
+        tot += 1
+    else:
+        print('\033[31m', end='')
+    print('{} '. format(c), end='')
+print(f'\n\033[mO número {num} foi divisível {tot} vezes')
+if tot == 2:
+    print('E por isso ele É PRIMO!')
 else:
-    print(f"O número {num} não é primo!")
+    print('E por isso ele NÃO É PRIMO!')
